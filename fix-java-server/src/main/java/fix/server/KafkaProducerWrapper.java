79c6486fbd7af25762b6d260436c3849b129789a
Samuel Guillemet <samuel.guillemet@telecom-sudparis.eu>
2023-11-21 14:32:08 +0100

    🐛 Fix KafkaProducerWrapper to include key in ProducerRecord

--- fix-java-server/src/main/java/fix/server/KafkaProducerWrapper.java
+++ fix-java-server/src/main/java/fix/server/KafkaProducerWrapper.java
@@ -1,20 +1,36 @@
 package fix.server;
 
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
-import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.IntegerSerializer;
 
-import java.util.Properties;
-import fix.avro.Order;
 import fix.config.BasicConfig;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import io.confluent.kafka.serializers.KafkaAvroSerializerConfig;
+import pfe_broker.avro.Order;
 
 public class KafkaProducerWrapper {
     private final String bootstrapServers;
     private final String topic;
     private final Producer<Integer, Order> producer;
+
+    private Callback callback = new Callback() {
+        @Override
+        public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+            if (e == null) {
+                System.out.printf("Produced record to topic %s partition [%d] @ offset %d%n", recordMetadata.topic(),
+                        recordMetadata.partition(), recordMetadata.offset());
+            } else {
+                e.printStackTrace();
+            }
+        }
+    };
 
     public KafkaProducerWrapper(String bootstrapServers, String topic) {
         this.bootstrapServers = bootstrapServers;
@@ -27,14 +43,14 @@ public class KafkaProducerWrapper {
         properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class.getName());
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class.getName());
-        properties.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, BasicConfig.SCHEMA_REGISTRY_URL);
-    
-        return new org.apache.kafka.clients.producer.KafkaProducer<>(properties);
+        properties.put(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, BasicConfig.SCHEMA_REGISTRY_URL);
+
+        return new KafkaProducer<>(properties);
     }
 
-    public void publishMessage(Order value) {
-        ProducerRecord<Integer, Order> record = new ProducerRecord<>(topic, value);
-        producer.send(record);
+    public void publishMessage(Order value, Integer key) {
+        ProducerRecord<Integer, Order> record = new ProducerRecord<>(topic, key, value);
+        producer.send(record, callback);
         producer.flush();
     }
 
