521663bcace8cc6311fe2012ce3096b9e83d8006
TJA <theojosephantoine@gmail.com>
2023-11-21 11:51:07 +0100

    Simplified the relay part of the Application processors are unecessary, overloading is better

--- fix-java-server/src/main/java/fix/relay/processors/RecordProcessor.java
+++
@@ -1,9 +0,0 @@
-package fix.relay.processors;
-
-import org.json.JSONObject;
-
-import fix.relay.Application;
-
-public interface RecordProcessor {
-    void processRecord(JSONObject jsonObject, Application application);
-}
